4f569082038ba4d258b7934f809eaa9fa5d3f054
Johannes Plunien <johannes.plunien@xing.com>
2009-12-28 13:48:43 +0100

    Use MX::Getopt in example script

--- examples/test.pl
+++ examples/test.pl
@@ -5,7 +5,7 @@ use FindBin;
 use lib ( "$FindBin::Bin/../lib", "$FindBin::Bin/lib" );
 use Gearman::Driver;
 
-my $driver = Gearman::Driver->new(
+my $driver = Gearman::Driver->new_with_options(
     namespaces => [qw(GDExamples)],
     server     => 'localhost:4730,localhost:4731',
     interval   => 3,
