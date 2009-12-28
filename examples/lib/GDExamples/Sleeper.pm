f38fb86fd1baabd2be4aa47059e08dd49302ad2f
Johannes Plunien <johannes.plunien@xing.com>
2009-12-28 18:31:35 +0100

    Extended Sleeper example

--- examples/lib/GDExamples/Sleeper.pm
+++ examples/lib/GDExamples/Sleeper.pm
@@ -4,7 +4,7 @@ package    # hide from PAUSE
 use base qw(Gearman::Driver::Worker);
 use Moose;
 
-sub ZzZzZzzz : Job : MinChilds(5) : MaxChilds(10) {
+sub ZzZzZzzz : Job : MinChilds(3) : MaxChilds(6) {
     my ( $self, $driver, $job ) = @_;
     my $time = 2;
     sleep($time);
@@ -14,6 +14,13 @@ sub ZzZzZzzz : Job : MinChilds(5) : MaxChilds(10) {
 sub output {
     my ( $self, $workload ) = @_;
     print "$workload\n";
+}
+
+sub long_running_ZzZzZzzz : Job : MinChilds(1) : MaxChilds(2) {
+    my ( $self, $driver, $job ) = @_;
+    my $time = 4;
+    sleep($time);
+    $self->output( $job->workload );
 }
 
 1;
