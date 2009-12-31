346a5157815957f982b3b5099bf3123777a891b1
Johannes Plunien <johannes.plunien@xing.com>
2009-12-31 14:55:57 +0100

    Fixed ISA check

--- lib/Gearman/Driver.pm
+++ lib/Gearman/Driver.pm
@@ -530,6 +530,7 @@ sub _load_namespaces {
 sub _is_valid_worker_subclass {
     my ( $self, $module ) = @_;
     return 0 unless $module->can('meta');
+    return 0 unless $module->meta->can('linearized_isa');
     return 0 unless grep $_ eq 'Gearman::Driver::Worker', $module->meta->linearized_isa;
     return 1;
 }
