d7b4046327528ae74848a3833d2105d6139343cd
Johannes Plunien <johannes.plunien@xing.com>
2010-01-22 13:32:17 +0100

    First draft of set_min/max_childs + status

--- lib/Gearman/Driver/Console.pm
+++ lib/Gearman/Driver/Console.pm
@@ -2,10 +2,15 @@ package Gearman::Driver::Console;
 
 use Moose;
 use POE qw(Component::Server::TCP);
+use Try::Tiny;
 
 =head1 NAME
 
 Gearman::Driver::Console - Management console
+
+=head1 SYNOPSIS
+
+TODO: Add docs | telnet localhost 47300 etc
 
 =head1 DESCRIPTION
 
@@ -40,10 +45,80 @@ sub BUILD {
         Port        => $self->port,
         ClientInput => sub {
             my ( $session, $heap, $input ) = @_[ SESSION, HEAP, ARG0 ];
-            print "Session ", $session->ID(), " got input: $input\n";
-            $heap->{client}->put($input);
+            my ( $command, @params ) = split /\s+/, $input;
+            if ( $self->can($command) ) {
+                try {
+                    my @result = $self->$command(@params);
+                    $heap->{client}->put($_) for @result;
+                    $heap->{client}->put('.');
+                }
+                catch {
+                    chomp($_);
+                    $heap->{client}->put($_);
+                };
+            }
+            else {
+                $heap->{client}->put("ERR unknown_command: $command");
+            }
         }
     );
+}
+
+=head1 COMMANDS
+
+=head2 status
+
+  name min_childs max_childs current_childs
+
+=cut
+
+sub status {
+    my ($self) = @_;
+    my @result = ();
+    foreach my $job ( $self->driver->get_jobs ) {
+        push @result, sprintf( "%s\t%d\t%d\t%d", $job->name, $job->min_childs, $job->max_childs, $job->count_childs );
+    }
+    return @result;
+}
+
+=head2 set_min_childs
+
+  set_min_childs $job_name $min_childs_value
+
+=cut
+
+sub set_min_childs {
+    my ( $self, $job_name, $min_childs ) = @_;
+
+    if ( !defined($min_childs) or $min_childs !~ /^\d+$/ or $min_childs < 0 ) {
+        die "ERR invalid_value: minchilds must be greater than 0\n";
+    }
+
+    my $job = $self->driver->get_job($job_name) || die "ERR invalid_job_name: $job_name\n";
+
+    $job->min_childs($min_childs);
+
+    return "OK";
+}
+
+=head2 set_max_childs
+
+  set_max_childs $job_name $max_childs_value
+
+=cut
+
+sub set_max_childs {
+    my ( $self, $job_name, $max_childs ) = @_;
+
+    if ( !defined($max_childs) or $max_childs !~ /^\d+$/ or $max_childs < 0 ) {
+        die "ERR invalid_value: max_childs must be >= 0\n";
+    }
+
+    my $job = $self->driver->get_job($job_name) || die "ERR invalid_job_name: $job_name\n";
+
+    $job->min_childs($min_childs);
+
+    return "OK";
 }
 
 no Moose;
