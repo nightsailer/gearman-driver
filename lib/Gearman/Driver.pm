a6f11c8d56bbfe05559bdac03eeeade56194add0
Johannes Plunien <johannes.plunien@xing.com>
2010-02-02 21:34:51 +0100

    Checking in changes prior to tagging of version 0.01021.  Changelog diff is:

--- lib/Gearman/Driver.pm
+++ lib/Gearman/Driver.pm
@@ -12,7 +12,7 @@ use MooseX::Types::Path::Class;
 use POE;
 with qw(MooseX::Log::Log4perl MooseX::Getopt Gearman::Driver::Loader);
 
-our $VERSION = '0.01020';
+our $VERSION = '0.01021';
 
 =head1 NAME
 
