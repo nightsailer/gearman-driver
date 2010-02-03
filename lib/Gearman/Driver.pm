fc8fdd3277643bb26e181ea582a89de786b6edb8
Johannes Plunien <johannes.plunien@xing.com>
2010-02-03 23:58:06 +0100

    Checking in changes prior to tagging of version 0.01022.  Changelog diff is:
    diff --git a/Changes b/Changes
    index d3f8c7a..88e84dd 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,6 @@
     Revision history for Gearman-Driver
    
    +0.01022     Wed Feb 03 2010
                 - Child/parent communication using unix socket now
                   instead of Cache::FastMmap

--- lib/Gearman/Driver.pm
+++ lib/Gearman/Driver.pm
@@ -12,7 +12,7 @@ use MooseX::Types::Path::Class;
 use POE;
 with qw(MooseX::Log::Log4perl MooseX::Getopt Gearman::Driver::Loader);
 
-our $VERSION = '0.01021';
+our $VERSION = '0.01022';
 
 =head1 NAME
 
